35682845b0066a11d4461382b2968d64119606a7
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-08-11 12:59:21 -0400

    Adding Wednesday homework progress

--- queries.sql
+++ queries.sql
@@ -51,13 +51,65 @@ Add a column to the table: ParkingSpot as textual information that can store up 
 
 ALTER TABLE "Employees" ADD COLUMN "ParkingSpot" VARCHAR(10);
 
-Add queries to queries.sql that show how to:
+******WEDNESDAY HOMEWORK******
 
-Recreate your database and tables and use NOT NULL for columns you feel are required.
+In your CompanyDatabase, add a table named Departments with the following columns:
+Id as a primary key
+DepartmentName as text
+Building as text
+
+CREATE TABLE "Departments" (
+................   "Id" Serial Primary Key,
+................   "Department Name" TEXT);
+
+Forgot the Builing column so Altered Table:
+
+ALTER TABLE "Departments" ADD COLUMN "Building" TEXT;
+
+Add a Foreign key DepartmentId to your Employees Table. If you have trouble, remove the existing employees by running truncate table "Employees".
+
+ALTER TABLE "Employees" ADD COLUMN "DepartmentId" INTEGER NULL REFERENCES "Departments" ("Id");
+
+Add tables named Products and Orders.
+Products should have the columns
+Id as a primary key
+Price as a data type that can store a number with decimal places (e.g. 45.12)
+Name as textual data
+Description as textual data
+QuantityInStock as a data type that can store a number WITHOUT decimal places (e.g. 42)
+
+CREATE TABLE "Products" ("Id" Serial Primary Key,"Price" DECIMAL,"Name" TEXT,"Description" TEXT,"QuantityInStock" INT);
 
 
+Orders should have the columns
+Id as a primary key
+OrderNumber as textual data
+DatePlaced as a data type that can store a date (with Year, Month, and Day) -- or one with hours, minutes, and seconds. Student's choice.
+Email as textual data
+In our company, one Order can have many Products and one Product can have many Orders. This will be a Many-to-Many relationship. Create the necessary table ProductOrders, foreign keys, and the OrderQuantity field needed for this to happen.
+[ ] Create queries that can do the following:
 
-Add a primary key named Id to your table.
+Insert the following Departments
+Department Name	Building
+Development	Main
+Marketing	North
+Insert the following Employees
+FullName	Salary	JobPosition	PhoneExtension	IsPartTime	Department Id
+Tim Smith	40000	Programmer	123	false	1
+Barbara Ramsey	80000	Manager	234	false	1
+Tom Jones	32000	Admin	456	true	2
+Insert the following Products
+
+Price	Name	Description	QuantityInStock
+12.45	Widget	The Original Widget	100
+99.99	Flowbee	Perfect for haircuts	3
+[ ] Insert a new order with order number X529, placed on Jan 1st, 2020 at 4:55PM, by someone with the email address "person@example.com"
+[ ] Add an order quantity of 3 for the product named Widget to the order X529
+[ ] Add an order quantity of 2 for the product named Flowbee to the order X529
+[ ] Given a building, return all employees that work in that building. Show this query for buildings named North Side, East Side, and finally a building that you actually have in your data -- even if your data doesn't have any departments in those buildings. NOTE this means you can't manually look up Ids and use them, you have to let SQL do that work. (Hint: JOIN)
+[ ] Find all orders that contain the product id of 2.
+[ ] Find the quantity of the Flowbee product from order with order number X529. You may not write the value of an Id in your query, let SQL do the work (HINT: JOIN)
+
 
 
 
